7512b10f150717ff7a7c7255097885e221539ccc
inanamm <martina.holenstein@gmail.com>
2023-01-07 23:16:13 +0100

    Initial Commit

---
+++ script/main.js
@@ -0,0 +1,9 @@
+// $('document').ready( function(){
+//     /*$('.trigger').on('mousewheel DOMMouseScroll', function(){
+//         $(this).next('section').animate({scrollTop:0}, 500); */
+//     $('li').width()
+// });
+// });
+
+/*Marquee3k.init()
+*/
